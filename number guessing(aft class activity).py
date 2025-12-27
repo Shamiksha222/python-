942ea72520b07aeed055a6c16b2af5ae28e2b165
Shamiksha222 <sathiyamurthy_shamiksha@students.edu.sg>
2025-12-27 21:23:41 +0800

    Update number guessing(aft class activity).py

--- number guessing(aft class activity).py
+++ number guessing(aft class activity).py
@@ -1,4 +1,4 @@
-#number chose by judge
+#number chosen by judge
 n = 25
 
 print("Welcome to the coding competition!")
@@ -6,12 +6,13 @@ print("Welcome to the coding competition!")
 
 while True:
     guess= int(input("Enter your guess:"))
-
-    if guess > n :
-        print("your guess is higher then the correct value")
-    elif guess < n :
-        print("your number is lower than the correct value")
-    else:
-        print("You got the correct ans!")
+    
+if guess > n :
+    print("your guess is higher then the correct value")
+elif guess < n :
+    print("your number is lower than the correct value")
+else:
+    print("You got the correct ans!")
        
+
     
